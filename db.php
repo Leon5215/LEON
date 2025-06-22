360978b2ed24400987ccd71e06f6f555ca28a8a2
nkuuts345211 <aistu112534@gmail.com>
2025-06-22 23:31:20 +0800

    Add files via upload

--- db.php
+++ db.php
@@ -1,14 +1,5 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "leon";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("連線失敗: " . $conn->connect_error);
-}
-
-$conn->set_charset("utf8mb4");
+$link=mysqli_connect("127.0.0.1","root","","leon");
+mysqli_set_charset($link,"set charset utf8");
+session_start();
 ?>
